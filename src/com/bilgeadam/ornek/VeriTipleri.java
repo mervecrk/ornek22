b79de4d79b7023b1f98570f0e9c229fed04bdb20
Bora Yüret <borayuret@gmail.com>
2021-11-14 14:31:07 +0300

    Veritipleri eklendi

---
+++ src/com/bilgeadam/ornek/VeriTipleri.java
@@ -0,0 +1,45 @@
+package com.bilgeadam.ornek;
+
+public class VeriTipleri {
+
+    public static void main(String[] args) {
+
+        int yas = 60;
+
+        char cinsiyet = 'E';
+
+        String sehir = "Ankara";
+
+
+        if (yas<35)
+        {
+            System.out.println("Genç");
+        }
+        else
+        {
+            System.out.println("Yaşlı");
+        }
+
+        // i++  ==> i = i+1
+        for (int i=1; i<=10; i++)
+        {
+            System.out.println(i+"-Merhaba");
+        }
+
+        System.out.println("Şehir index:" + sehir.indexOf("A"));
+
+        if(sehir.indexOf("A") >= 0)
+        {
+            System.out.println("İçinde A harfi var!");
+        }
+
+        String deneme = "Bora Yüret";
+
+        System.out.println(deneme.substring(5));
+
+
+
+    }
+
+
+}
