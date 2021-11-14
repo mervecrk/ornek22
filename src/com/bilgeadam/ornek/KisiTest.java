b715050b7eefb51edeeb7662dcfb7a990bd1bba9
Bora Yüret <borayuret@gmail.com>
2021-11-14 13:33:21 +0300

    KisiTest güncellendi

--- src/com/bilgeadam/ornek/KisiTest.java
+++ src/com/bilgeadam/ornek/KisiTest.java
@@ -3,6 +3,9 @@ package com.bilgeadam.ornek;
 public class KisiTest {
     public static void main(String[] args) {
 
+        Kisi kisi = new Kisi(1, "Cahit", "Arf", 5000);
+        System.out.println(kisi);
+
 
     }
 }
