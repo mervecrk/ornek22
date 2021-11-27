ad1047c92ad29bf01c98e3bf295656c473d0321a
Bora Yüret <borayuret@gmail.com>
2021-11-27 13:53:25 +0300

    ReadFromConsoleSon yaş yanlış girilince tekrar soracak hale getirildi.

--- src/com/bilgeadam/ornek/ReadFromConsoleSon.java
+++ src/com/bilgeadam/ornek/ReadFromConsoleSon.java
@@ -1,18 +1,19 @@
 package com.bilgeadam.ornek;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ReadFromConsoleSon {
 
-    String isim;
-    int yas;
+    static String isim;
+    static int yas;
 
     Scanner sc = new Scanner(System.in);
 
     public String isimSor()
     {
         String ad = "";
-        System.out.println("İsminizi giriniz:");
+        System.out.print("İsminizi giriniz:");
         ad = sc.nextLine();
 
         return ad;
@@ -21,11 +22,19 @@ public class ReadFromConsoleSon {
 
     public int yasSor()
     {
-        int localYas = 0;
-        System.out.println("Yaşınızı giriniz:");
-        localYas = sc.nextInt();
+        try {
+            int localYas = 0;
+            System.out.print("Yaşınızı giriniz:");
+            localYas = Integer.parseInt(sc.nextLine());
 
-        return localYas;
+            return localYas;
+        }
+        catch(NumberFormatException e)
+        {
+            System.err.println("Hata: Yaş alanına sayı girmek zorundasınız!");
+            System.out.println();
+            return -99;
+        }
 
     }
 
@@ -43,8 +52,14 @@ public class ReadFromConsoleSon {
     public static void main(String[] args) {
         ReadFromConsoleSon son = new ReadFromConsoleSon();
 
-        son.isimSor();
-        son.yasSor();
+        isim = son.isimSor();
+        yas = son.yasSor();
+
+        while(yas < 0)
+        {
+            yas = son.yasSor();
+        }
+
         son.sonucYazdir();
 
 
